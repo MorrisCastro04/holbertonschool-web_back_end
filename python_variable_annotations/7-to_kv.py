9d4b0ebe2a12f0c66992405c814054c0f9e69bf3
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-24 09:20:44 -0400

    delete union in the tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,6 +5,6 @@ Takes a string and a number and returns a tuple
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """create a tuple with a str and a num"""
     return (k, v**2)
