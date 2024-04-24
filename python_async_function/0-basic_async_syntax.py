cdc1c41aa32bd6cb7edb80813aa0a03a8f1f0644
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-24 10:30:12 -0400

    wait a random period of time

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""funcion to wait a period of time"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """wait a random period of time"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
