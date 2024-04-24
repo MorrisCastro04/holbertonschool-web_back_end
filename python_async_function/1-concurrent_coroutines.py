8bb71c211e89854a98327226c1bf26fb1abd48fb
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-24 11:33:35 -0400

    sort Array

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -6,4 +6,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """storage the result in a List"""
     results = [await wait_random(max_delay) for _ in range(n)]
-    return results
+    return results.sort()
