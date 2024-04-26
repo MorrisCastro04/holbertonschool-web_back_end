3ecc6b4401b85f112aba7aa361c19352e261952e
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-04-26 15:00:05 -0400

    list with the funcion wait_random

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""list of wait random returns"""
+"""list with the funcion wait_random"""
 from typing import List
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    """storage the result in a List"""
-    results = [await wait_random(max_delay) for _ in range(n)]
-    return results
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """wait_n should return the list of all the delays (float values)"""
+    delays = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(delays)
