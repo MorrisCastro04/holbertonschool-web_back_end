aa9586488ef9633b05ec7971a3b724e4687818db
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-05-03 13:31:58 -0400

    change the collection to the if main

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-""" Log stats """
+""" log stats """
 from pymongo import MongoClient
 
 
+Methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+def log_stats(mongo_collectiion):
+    """ provides stats """
+    items = {}
+
+    result = mongo_collectiion.count_documents(items)
+    print(f"{result} logs")
+    print("Methods:")
+    for method in Methods:
+        log_stats(collection, method)
+    status = collection.count_documents({"path": "/status"})
+    print (f"{status} status check")
+
+
 if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db_nginx = client.logs.nginx
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    count_logs = db_nginx.count_documents({})
-    print(f'{count_logs} logs')
-
-    print('Methods:')
-    for method in methods:
-        count_method = db_nginx.count_documents({'method': method})
-        print(f'\tmethod {method}: {count_method}')
-
-    check = db_nginx.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-
-    print(f'{check} status check')
+    collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(collection)
