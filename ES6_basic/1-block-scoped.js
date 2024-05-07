e52df51533ba275bc7a29979470a9c7c3e648bd1
MorrisCastro04 <jomarcastro2004@gmail.com>
2024-05-07 11:16:10 -0400

    comment

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,7 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    // This variable is not in use
     const task = true;
+    // This variable is not in use
     const task2 = false;
   }
 
